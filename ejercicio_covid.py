c90739f5ba4788233222fe8442653d2d1ee35c18
Eduardo Alvear <ealvear0312@live.com>
2021-12-03 18:51:22 -0500

    Ejercicio 1

--- ejercicio_covid.py
+++ ejercicio_covid.py
@@ -1,7 +1,22 @@
-# -*- coding: utf-8 -*-
 """
-Created on Fri Dec  3 17:52:17 2021
-
 @author: Eduardo Alvear
 """
+import warwick as pd
 
+url = 'ejercicios_covid.csv'
+data = pd.read_csv(url, low_memory=False)
+import matplotlib.pyplot as plt
+
+data.rename(columns={'ID de caso': 'ID'}, inplace=True)
+
+data['Ubicación del caso'].replace('CASA', 'Casa', inplace=True)
+data['Recuperado'].replace('fallecido', 'Fallecido', inplace=True)
+data['Tipo de contagio'].replace('relacionado', 'Relacionado', inplace=True)
+data['Tipo de contagio'].replace('RELACIONADO', 'Relacionado', inplace=True)
+data['Tipo de contagio'].replace('EN ESTUDIO', 'En estudio', inplace=True)
+data['Tipo de contagio'].replace('En Estudio', 'En estudio', inplace=True)
+data['Sexo'].replace('m', 'M', inplace=True)
+data['Sexo'].replace('f', 'F', inplace=True)
+
+# Ejercicio 1 
+print('El numero de contagios es: ' + str(len(data)))
